44b79639680ed5f68aef68792d8aadb404e185cd
George Norris <georgenorris@georgenorris-k4lywk.local>
2020-03-08 17:51:54 -0500

    initial commit

---
+++ src/main/java/org/javaliftoff/Liftoff/project/controllers/HelloController.java
@@ -0,0 +1,15 @@
+package org.javaliftoff.Liftoff.project.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping
+    @ResponseBody
+    public String hello() {
+        return "Hello, world";
+    }
+}
---
+++ src/test/java/org/javaliftoff/Liftoff/project/LiftoffProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaliftoff.Liftoff.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftoffProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
